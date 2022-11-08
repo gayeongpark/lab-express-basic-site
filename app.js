403562990fae3e436bd51cd174fa45b620a2338f
GaYeong <rkdud5483@gmail.com>
2022-11-08 21:28:01 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// Require Express
+const express = require('express');
+
+// Express server handling requests and responses
+const app = express();
+
+// Make everything inside of public/ available
+app.use(express.static('public'));
+
+// About Route:
+app.get('/about', (request, response, next) => response.sendFile(__dirname + '/views/about.html'));
+
+// Home route:
+app.get('/home', (request, response, next) => response
+.sendFile(__dirname + '/views/home.html'));
+
+
+// Works route:
+app.get('/works', (request, response, next) => response.sendFile(__dirname + '/views/works.html'));
+
+// Server Started
+app.listen(3000, () => console.log('My first app listening on port 3000!'));
